8b1ff7f2712775f1a9867d1e13112d4efced30e2
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2024-11-04 00:14:37 +0530

    Update login.jsx

--- expense/Frontend/src/Components/login.jsx
+++ expense/Frontend/src/Components/login.jsx
@@ -23,7 +23,7 @@ function Login() {
         setLoading(true);
 
         try {
-            const response = await fetch(`http://localhost:3000/api/auth/login`, {
+            const response = await fetch(`https://linkerweb-01.vercel.app/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
